a86ace693305a28a8f1fded85df23c85983ce0aa
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2024-11-28 10:43:23 +0100

    New stuff for vr

--- Assets/Scripts/NoteMapManager.cs
+++ Assets/Scripts/NoteMapManager.cs
@@ -1,22 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NoteMapManager : MonoBehaviour
 {
-        public float noteSpeed;
+    public float noteSpeed;
+    public bool gameHasStarted = false;
+    public float delayTimer;
+    public AudioSource Song;
+
+    void StartGame()
+    {
+        gameHasStarted = true;
+        Song.Play();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
         Application.targetFrameRate = 60;
+        delayTimer = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-                transform.position += new Vector3(0, 0, noteSpeed);
+        while (gameHasStarted == false)
+        {
+
+            delayTimer -= Time.deltaTime;
+        }
+
+        if (delayTimer <= 0)
+        {
+            StartGame();
+        }
+
+        if (gameHasStarted)
+        {
+
+            transform.position += new Vector3(0, 0, noteSpeed);
+        }
 
     }
 }
