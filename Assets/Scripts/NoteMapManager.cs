dcdf6eaae8ae6fc140f55dc790a884a3a0268988
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2024-11-29 12:59:34 +0100

    fixed maybe

--- Assets/Scripts/NoteMapManager.cs
+++ Assets/Scripts/NoteMapManager.cs
@@ -6,15 +6,30 @@ using UnityEngine.InputSystem;
 
 public class NoteMapManager : MonoBehaviour
 {
-    public float noteSpeed;
-    public bool gameHasStarted = false;
-    public float delayTimer;
-    public AudioSource Song;
+    //------------------------------------------------
 
-    void StartGame()
+    public float startDistance = 0.948f;
+    public float delayDistance;
+    public float delayTimer;
+    public float noteSpeed;
+    public float songNoteSpeed = 0.052f;
+    public float delayNoteSpeed;
+    public float noteDistance = 0.54f;
+    float notesPerSecond = 2.16f;
+
+
+    //--------------------------------------------------
+    public bool gameHasStarted = false;
+    public int songplayedamount;
+    public AudioSource Song;
+    public AudioClip clip;
+    public float volume = 0.1f;
+
+    public void StartGame()
     {
         gameHasStarted = true;
-        Song.Play();
+        Song.PlayOneShot(clip, volume);
+        noteSpeed = songNoteSpeed;
     }
 
     // Start is called before the first frame update
@@ -22,26 +37,41 @@ public class NoteMapManager : MonoBehaviour
     {
         QualitySettings.vSyncCount = 0; // Set vSyncCount to 0 so that using .targetFrameRate is enabled.
         Application.targetFrameRate = 60;
-        delayTimer = 5;
+
+        //-------------------------------------------------------------------------------------
+
+        delayTimer = 7;
+
+        delayDistance = -14;
+
+        delayNoteSpeed = delayDistance / delayTimer *-1 * 0.04f * 0.5f ;
+        
+        transform.position = new Vector3(-0.882f, 0.948f, delayDistance);
+        
+        noteSpeed = delayNoteSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        while (gameHasStarted == false)
+        if (delayTimer > 0)
         {
-
             delayTimer -= Time.deltaTime;
-        }
 
-        if (delayTimer <= 0)
+        }
+        
+        if(delayTimer <= 1 && songplayedamount < 1)
         {
             StartGame();
+            songplayedamount += 1;
         }
 
         if (gameHasStarted)
         {
-
+            transform.position += new Vector3(0, 0, noteSpeed);
+        }
+        else if(gameHasStarted == false)
+        {
             transform.position += new Vector3(0, 0, noteSpeed);
         }
 
